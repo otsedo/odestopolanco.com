3d6a975a74a469053075a5657e5ff15ec053d894
Odesto Polanco <38387829+otsedo@users.noreply.github.com>
2023-11-20 16:51:37 -0400

    Updating logo to SVG

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,14 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import NavLinks from './NavLinks'
+import Logo from '../public/assets/img/logo.svg'
 
 const Navbar = () => {
   return (
     <div className='flex justify-between items-center w-full h-40 px-2 md:px-10 fixed'>
       <div className="logo-container">
         <Link href='/'>
-          <Image alt='odesto polanco logo' src='/assets/img/logo.png' height={100} width={200} />
+          <Image alt='odesto polanco logo' src={Logo} height={100} width={200} />
         </Link>
       </div>
       <NavLinks />
