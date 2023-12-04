bd91637ffc317731ff184ab273e4e356d83aae63
Odesto Polanco <38387829+otsedo@users.noreply.github.com>
2023-12-04 16:59:07 -0400

    corrgieindo footer

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -3,14 +3,14 @@ import { socialLinks, footerLinks } from '../../lib/constants'
 
 const Footer = () => {
   return (
-    <div className='sm:flex-col m-9 md:grid md:grid-cols-3 md:justify-between px-20 '>
+    <div className='sm:flex-col m-9 md:grid md:grid-cols-3 md:justify-between md:px-20 mt-20'>
 
-      <div className='sm:mt-4 '>
-        <p className='text-xl my-4'>Odesto Polanco</p>
-        <p className='text-base my-4 max-w-md'>Mi nombre es Mi nombre es <span className='text-secondaryBlue'>Odesto Polanco Rodríguez</span>, ingeniero en sistemas de la República Dominicana en plena transición de Backend Developer a Full Stack Developer.</p>
+      <div className='sm:mt-4 sm:w-full'>
+        <p className='text-xl my-4 sm:w-full text-center'>Odesto Polanco</p>
+        <p className='text-base my-4 '>Mi nombre es Mi nombre es <span className='text-secondaryBlue'>Odesto Polanco Rodríguez</span>, ingeniero en sistemas de la República Dominicana en plena transición de Backend Developer a Full Stack Developer.</p>
       </div>
 
-      <div className='sm:my-2'>
+      <div className='sm:my-2 mt-10'>
         <h3 className='text-xl my-3 text-center'>Redes Sociales</h3>
         <div className='flex text-3xl p-4 gap-7 pb-8  justify-center'>
           {socialLinks.map((link) => (
@@ -19,7 +19,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className='flex flex-col justify-center align-middle text-center'>
+      <div className='flex flex-col justify-center align-middle text-center mt-6'>
         <h3 className='text-xl my-4'>Enlasces rápidos</h3>
         <div className='flex flex-col text-lg'>
           {footerLinks.map((link) => (
