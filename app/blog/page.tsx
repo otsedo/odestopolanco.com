be6bbaa69eae2deffc41ffe67ed6edfdc71ddb85
Odesto Polanco <38387829+otsedo@users.noreply.github.com>
2023-12-06 23:35:05 -0400

    fix type

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,7 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 import { useCallback, useEffect, useState } from "react";
 
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL || '', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '');
 
 interface BlogPost {
   id: number;
