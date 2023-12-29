b79d3199c635f6984b73272798535ed0bfd1214e
Odesto Polanco <38387829+otsedo@users.noreply.github.com>
2023-12-28 20:19:32 -0400

    Agregando Google Analythics

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 
 import Hero from "@/components/sections/Hero";
 import Skills from "@/components/sections/skills";
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <main>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} strategy="lazyOnload" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+
+
       <Hero />
       <Skills />
 
